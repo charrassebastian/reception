8edbd69668c1b1c9958461c794101bd0d2daf953
Sebastian Horacio Charras <sebastiancharras@outlook.com>
2023-06-29 16:53:58 -0300

    Iniciada implementacion de useFetch

--- src/hooks/fetching/fetch/useFetch.js
+++ src/hooks/fetching/fetch/useFetch.js
@@ -1,6 +1,18 @@
-import { renderHook } from '@testing-library/react';
 import { useEffect, useState } from 'react';
 
 export function useFetch(url) {
-    return null;
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        if(!url){
+            setData(null);
+        } else {
+            setInterval(async () => {
+                const res = await fetch(url);
+                setData(res);
+            }, 1000);
+        }
+    }, [url]);
+    return { data, error, loading };
 }

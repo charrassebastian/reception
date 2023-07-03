fdeb4745a473d03e4230020801d2b85e28ce7be4
= <=>
2023-07-02 21:20:42 -0300

    Modificado useFetch, el problema de data null persiste

--- src/hooks/fetching/fetch/useFetch.js
+++ src/hooks/fetching/fetch/useFetch.js
@@ -1,5 +1,11 @@
 import { useEffect, useState } from 'react';
 
+async function updateData (url) {
+    const fetched = await fetch(url);
+    const res = await fetched.json();
+    setData(res);
+}
+
 export function useFetch(url) {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
@@ -8,11 +14,10 @@ export function useFetch(url) {
         if(!url){
             setData(null);
         } else {
-            setInterval(async () => {
-                const res = await fetch(url);
-                setData(res);
-            }, 1000);
+            // add interval for polling the data
+            updateData(url);
         }
+        // add clear function for the interval when it is implemented
     }, [url]);
     return { data, error, loading };
 }
--- src/hooks/fetching/fetch/useFetch.test.js
+++ src/hooks/fetching/fetch/useFetch.test.js
@@ -1,30 +1,48 @@
 import '@testing-library/jest-dom';
 import { useFetch } from './useFetch';
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 describe('useFetch', () => {
-    beforeAll(() => {
-        jest.useFakeTimers;
+    beforeEach(() => {
+        jest.useFakeTimers();
     });
-    afterAll(() => {
-        jest.clearAllTimers;
-        jest.useFakeTimers;
+    afterEach(() => {
+        jest.clearAllTimers();
+        jest.useRealTimers();
     });
     it('should contain null for the data property of the returned object when no arguments are passed to it', () => {
         const { result } = renderHook(useFetch);
         expect(result.current.data).toBeNull();
     });
-    it('should return the fetched data when an error does not happen', () => {
+    it('should return the fetched data when an error does not happen', async () => {
         const expectedTrivia = [{ id: 1, question: 'question 1', options: [{ id: 1, text: 'option 1 for trivia 1' }, { id: 2, text: 'option 2 for trivia 1' }, { id: 3, text: 'option 3 for trivia 1' },], explanation: 'explanation 1' },
         { id: 2, question: 'question 2', options: [{ id: 4, text: 'option 1 for trivia 2' }, { id: 5, text: 'option 2 for trivia 5' }, { id: 3, text: 'option 6 for trivia 2' },], explanation: 'explanation 2' }];
         global.fetch = jest.fn(() =>
             Promise.resolve({
-                json: () => Promise.resolve({})
+                json: () => Promise.resolve(expectedTrivia)
             })
         );
-        const { result } = renderHook(useFetch, 'url');
-        jest.advanceTimersByTime(1000);
-        const { data } = result.current;
-        expect(data).toEqual(expectedTrivia);
+        /* only for making sure the mock works */
+        const tempFunc = async () => {
+            try {
+                const fetched = await fetch('url');
+                return await fetched.json();
+            } catch (error) {
+                return null;
+            }
+        }
+        const pretendedResult = await tempFunc();
+        expect(pretendedResult).toEqual(expectedTrivia);
+        /* ----------------------------------- */
+        const url = 'url';
+        const { result } = renderHook(useFetch, url);
+        const data1 = result.current.data;
+        //act(() => jest.advanceTimersByTime(2000));
+        const data2 = result.current.data;
+        console.log('data1: ' + data1);
+        console.log('data2: ' + data2);
+        expect(fetch).toHaveBeenLastCalledWith(url);
+        expect(data2).toEqual(expectedTrivia);
+        //expect(data).toEqual(expectedTrivia);
     });
     it('should return null wen an error occurs', () => {
 

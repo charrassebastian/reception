51f69ea73bdd6a98a0089dfc54153f71bb61751a
Sebastian Horacio Charras <sebastiancharras@outlook.com>
2023-07-07 15:38:00 -0300

    Agregado router, ReceptionInformation, navbar y text to speech

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { TriviaElementsEditor } from './triviaElementsEditor/TriviaElementsEditor';
+import { SpotsEditor } from './spotsEditor/SpotsEditor';
+import { ReceptionInformation } from './receptionInformation/ReceptionInformation';
+import { NavigationBar } from './navigationBar/NavigationBar';
 
 const queryClient = new QueryClient();
 
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <div data-testid="app">
-      </div>
+export default function App() {
+      <QueryClientProvider client={queryClient}>
+        <NavigationBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ReceptionInformation />} />
+            <Route path="/triviaEditor" element={<TriviaElementsEditor />} />
+            <Route path="/spotEditor" element={<SpotsEditor />} />
+          </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
+  return (
+    <div data-testid="app">
+      hola
+    </div>
   )
 }
-
-export default App
---
+++ src/receptionInformation/ReceptionInformation.jsx
@@ -0,0 +1,11 @@
+import { Trivia } from '../trivia/Trivia';
+import { Spots } from '../spots/Spots';
+
+export function ReceptionInformation(){
+    return (
+        <div>
+            <Trivia></Trivia>
+            <Spots></Spots>
+        </div>
+    );
+}

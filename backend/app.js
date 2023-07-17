259444185ee599c10dade5286d3bb37a2f9d5133
Sebastian Charras <sebastianhoraciocharras@gmail.com>
2023-07-17 21:26:47 +0000

    Ahora el servidor funciona cuando se lo ejecuta con node server.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const { router } = require('./routes')
+require('dotenv').config()
 app.use(express.json())
 app.use(router)
 module.exports.app = app
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const { app } = require('./app')
+const { connect, getUri, closeDb } = require('./db')
 const port = 8090
 
+const run = async () => {
+    const user = process.env.DB_USER
+    const password = process.env.DB_PASSWORD
+    const uri = 'mongodb://' + user + ':' + password + '@127.0.0.1:27017'
+    await connect({ uri })
+}
+
 if (require.main === module) {
-    app.listen(() => console.log('Listening on port ' + port))
+    run()
+    app.listen(port, () => console.log('Listening on port ' + port))
 }
 
 module.exports.app = app

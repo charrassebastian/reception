32f00287cb15fe2966eff056629d86cb57449c6f
Sebastian Charras <sebastianhoraciocharras@gmail.com>
2023-07-17 16:19:32 +0000

    Agregado test para UPDATE de trivia

--- backend/__tests__/trivia.test.js
+++ backend/__tests__/trivia.test.js
@@ -44,6 +44,31 @@ describe('POST /trivia', () => {
 
         id = response.body[0]._id
     })
+    test('should update the trivia', async () => {
+        const updatedTrivia = {
+            question: 'updated question',
+            explanation: 'updated explanation',
+            answers: [{id: 1, text: 'first answer', isCorrect: true}]       
+        }
+
+        const firstResponse = await request(app)
+        .put('/trivia/' + id)
+        .send(updatedTrivia)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        
+        const secondResponse = await request(app)
+        .get('/trivia/')
+        .expect('Content-Type', /json/)
+        .expect(200)
+
+        expect(secondResponse.body).toHaveLength(1)
+        expect(secondResponse.body[0].question).toEqual(updatedTrivia.question)
+        expect(secondResponse.body[0].explanation).toEqual(updatedTrivia.explanation)
+        expect(secondResponse.body[0].answers).toHaveLength(1)
+        expect(secondResponse.body[0].answers[0]).toEqual(updatedTrivia.answers[0])
+    })
     test('should delete the new trivia', async () => {
         const firstResponse = await request(app)
         .delete('/trivia/' + id)

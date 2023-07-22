0a451d7722967b794fbbbde5b42d0fd5e74966fa
Sebastian Charras <sebastianhoraciocharras@gmail.com>
2023-07-22 00:24:00 +0000

    Modificada trivia answers section para poder mostrar mas respuestas

--- frontend/src/triviaAnswersSection/TriviaAnswersSection.jsx
+++ frontend/src/triviaAnswersSection/TriviaAnswersSection.jsx
@@ -1,8 +1,8 @@
 export function TriviaAnswersSection({ answers }){
     return (
         <div className="flex flex-col h-full items-center justify-center">
-            <ul className="flex flex-row justify-around w-full">
-                {answers.map(answer => <li key={answer._id} className="rounded-md py-3 px-6 bg-white text-3xl">{answer.text}</li>)}
+            <ul className="flex flex-row flex-wrap justify-between w-full">
+                {answers.map(answer => <li key={answer._id} className="rounded-md m-5 py-3 px-6 bg-white text-3xl">{answer.text}</li>)}
             </ul>
         </div>
     );

2c2c525373da6f2e409fdfa7c4895482b528e7c1
Sebastian Charras <sebastianhoraciocharras@gmail.com>
2023-07-22 16:54:10 +0000

    Agregado soporte para entregara archivos estaticos (cliente) dede el backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const cors = require('cors')
 const { router } = require('./routes')
 require('dotenv').config()
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(router)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build'))
+})
 module.exports.app = app

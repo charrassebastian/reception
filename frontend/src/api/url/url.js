b419574e09dbaf7e4b6cc8361a7779bcd648b1a4
Sebastian Charras <sebastianhoraciocharras@gmail.com>
2023-07-23 20:18:01 +0000

    Arreglada url

--- frontend/src/api/url/url.js
+++ frontend/src/api/url/url.js
@@ -1,2 +1,1 @@
-//export const baseUrl = location.protocol + '//' + location.host + '/';
-export const baseUrl = 'http://89.116.212.198:8090/'
+export const baseUrl = location.protocol + '//' + location.host + '/';
